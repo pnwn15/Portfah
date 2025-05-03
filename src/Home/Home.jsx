19d705087c419aba509adc64a39c80f15134be5a
panawat <panawatsrimuang@gmail.com>
2025-05-03 10:11:30 +0700

    ggdh

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -11,13 +11,13 @@ function Home() {
       <section class="bg-white h-auto">
         <div class="grid max-w-screen-xl  px-4 py-12 mt-12 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-medium tracking-tight leading-none md:text-5xl xl:text-[20px] dark:text-white">
+            <h1 class="max-w-2xl mb-4 text-4xl font-medium tracking-tight leading-none md:text-5xl xl:text-[20px] ">
               Branding | Image making
             </h1>
-            <h1 class="max-w-2xl mb-4 text-4xl font-medium tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            <h1 class="max-w-2xl mb-4 text-4xl font-medium tracking-tight leading-none md:text-5xl xl:text-6xl ">
               Visual Designer
             </h1>
-            <p class="max-w-2xl mb-6 font-medium text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <p class="max-w-2xl mb-6 font-medium text-black lg:mb-8 md:text-lg lg:text-xl ">
               This is a template Figma file, turned into code using Anima. Learn
               more at AnimaApp.com
             </p>
@@ -25,7 +25,7 @@ function Home() {
               <Button name="Contact" />
             </div>
           </div>
-          <div class=" lg:mt-0 lg:col-span-5 lg:flex">
+          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
             <img
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
               alt="mockup"
